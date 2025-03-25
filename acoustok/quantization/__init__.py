4a20c5d9996655b49c52b17ff00c9ccd2003907c
Qu Wang <291733899+jamesa94@users.noreply.github.com>
2025-03-25 17:05:28 +0800

    quantization: export VQ classes from the subpackage

--- acoustok/quantization/__init__.py
+++ acoustok/quantization/__init__.py
@@ -1,1 +1,11 @@
 """Vector quantization: single codebooks and residual stacks."""
+
+from .residual_vq import QuantizeResult, ResidualVQ
+from .vq import EuclideanCodebook, VectorQuantizer
+
+__all__ = [
+    "EuclideanCodebook",
+    "VectorQuantizer",
+    "ResidualVQ",
+    "QuantizeResult",
+]
