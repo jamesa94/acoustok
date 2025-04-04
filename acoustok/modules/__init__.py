b0abe1350c5fbcf4f85e0c0734e8946060f3941f
Qu Wang <291733899+jamesa94@users.noreply.github.com>
2025-04-05 00:25:29 +0800

    modules: export conv, activations and seanet

--- acoustok/modules/__init__.py
+++ acoustok/modules/__init__.py
@@ -1,1 +1,25 @@
 """Neural building blocks: convolutions, activations and the SEANet codec body."""
+
+from .activations import Snake, make_activation
+from .conv import (
+    NormConv1d,
+    NormConvTranspose1d,
+    SConv1d,
+    SConvTranspose1d,
+    pad1d,
+    unpad1d,
+)
+from .seanet import SEANetDecoder, SEANetEncoder
+
+__all__ = [
+    "Snake",
+    "make_activation",
+    "NormConv1d",
+    "NormConvTranspose1d",
+    "SConv1d",
+    "SConvTranspose1d",
+    "pad1d",
+    "unpad1d",
+    "SEANetEncoder",
+    "SEANetDecoder",
+]
